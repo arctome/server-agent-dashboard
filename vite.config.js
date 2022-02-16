3375d628032dd5428b76ad8a89a8481eec22cc30
arctome <hi@arcto.me>
2022-02-16 11:23:34 +0000

    test: github action

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import compressDist from 'rollup-plugin-compress-dist'
 import react from '@vitejs/plugin-react'
+
+const compressOpts = {
+  type: 'tgz',
+  archiverName: 'pages.tar.gz',
+  sourceName: 'pages',
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compressDist(compressOpts)
+  ],
   preview: {
     port: 10081,
     host: "0.0.0.0"
